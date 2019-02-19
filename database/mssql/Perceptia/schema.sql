75c316564b951dec3f114b41f78ac2a8537fbb28
Christopher Kites <chriskites@gmail.com>
2019-02-19 10:40:09 -0800

    Add comments to schema AB#203

--- database/mssql/Perceptia/schema.sql
+++ database/mssql/Perceptia/schema.sql
@@ -2,17 +2,36 @@
 	Title: Perceptia Database Schema
 	Version: 0.1.1
 */
-USE master;
-GO
 
-IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Perceptia')
-	BEGIN
-		CREATE DATABASE Perceptia
-		COLLATE SQL_Latin1_General_CP1_CI_AS
-	END
+-------------------------------------------------------------------------------
+-- Setup Database --
+-------------------------------------------------------------------------------
+-- Select master to remove 
+USE [master]
 ;
 GO
 
+-- Remove existing Perceptia DB if applying schema again
+If Exists(Select name from master.dbo.sysdatabases Where Name = 'Perceptia')
+Begin
+	USE [master]
+	ALTER DATABASE [Perceptia] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE [Perceptia]
+End
+;
+GO
+
+-- Create Perceptia Database
+-- Use same Collate as Azure SQL
+CREATE DATABASE Perceptia
+	COLLATE Latin1_General_100_CI_AS_SC
+;
+GO
+
+-------------------------------------------------------------------------------
+-- Create Tables --
+-------------------------------------------------------------------------------
+-- Ensure Perceptia database is selected
 USE Perceptia
 ;
 GO
