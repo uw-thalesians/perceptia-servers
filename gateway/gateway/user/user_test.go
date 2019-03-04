2cf400009cd33bfc6450c5ff1af4234f5a216ea5
Christopher Kites <chriskites@gmail.com>
2019-03-04 15:16:17 -0800

    Prep for writing tests

--- gateway/gateway/user/user_test.go
+++ gateway/gateway/user/user_test.go
@@ -3,3 +3,16 @@
 package user
 
 // TODO: Write tests for user
+
+/*func TestNewUser_ValidateNewUser(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: test cases
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+
+		})
+	}
+}*/
