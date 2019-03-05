ba8522356ed91295a01cb8f8a4613e9b87d86ef6
Christopher Kites <chriskites@gmail.com>
2019-03-04 20:49:22 -0800

    Update setup-db.sh to test for skip env vars

--- database/mssql/setup-db.sh
+++ database/mssql/setup-db.sh
@@ -5,13 +5,17 @@
 
 SKIP_SETUP=$SKIP_SETUP
 
-if [ SKIP_SETUP -eq "Y"]
+echo "SKIP_SETUP=$SKIP_SETUP"
+echo "SKIP_SETUP_IF_EXISTS=$SKIP_SETUP_IF_EXISTS"
+
+if [ "$SKIP_SETUP" == "Y" ]
 then
         echo "SKIP_SETUP=Y, skipping setup-db.sh script"
         exit 0
 fi
 
 # wait for the SQL Server to come up
+echo "Sleeping for 30s to allow server to start"
 sleep 30s
 
 # Skip setup if database exists
@@ -19,7 +23,7 @@ sleep 30s
 
 SKIP_SETUP_IF_EXISTS=$SKIP_SETUP_IF_EXISTS
 
-if [ SKIP_SETUP_IF_EXISTS -eq "Y"]
+if [ "$SKIP_SETUP_IF_EXISTS" == "Y" ]
 then
         /opt/mssql-tools/bin/sqlcmd \
         -S localhost -U sa -P ${SA_PASSWORD} -d 'master' -b \
@@ -32,7 +36,9 @@ then
 fi
 
 # run the setup script to create the DB and the schema in the DB
+echo "Applying schema.sql to Perceptia database"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d master -i /script/schema.sql
 
 # run the setup script to create the stored procedures for the DB
+echo "Applying procedure.sql to Perceptia database"
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P ${SA_PASSWORD} -d master -i /script/procedure.sql
