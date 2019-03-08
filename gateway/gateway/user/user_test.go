2c26c2050037541e7d71e44eb2a6ad5365abeccf
Christopher Kites <chriskites@gmail.com>
2019-03-07 20:29:39 -0800

    Add several unit tests for pkg user AB#255

--- gateway/gateway/user/user_test.go
+++ gateway/gateway/user/user_test.go
@@ -6,6 +6,11 @@ import "testing"
 
 // TODO: Write tests for user
 
+// TestNewUser_ValidateNewUser is a unit test ensuring ValidateNewUser
+// check all fields of the struct.
+//
+// This test does not test all possible inputs; more exhaustive tests will
+// be run on the individual validation functions used by this method.
 func TestNewUser_ValidateNewUser(t *testing.T) {
 	tests := []struct {
 		name        string
@@ -26,12 +31,156 @@ func TestNewUser_ValidateNewUser(t *testing.T) {
 			"NewUser Valid. Should return nil",
 			false,
 		},
+		{"Missing Username",
+			&NewUser{
+				"",
+				"Test Tester",
+				"Test",
+				"$argon2id$v=19$m=65536,t=1," +
+					"p=2$9QWBW808Dq62aOfojSwBYg$muj80qzHXdB4DW06zgHbljhTLGrfFJ6qq8hDiK" +
+					"2esNTUBH0zlnBjGoDJ4S5glnKVCs9CD15cajhzkVxjgMwgHZdZ0JgZvUDcX9tJf8p7a" +
+					"TCOI8vN0vVVbZFbgUya6CFiqPH8cULMVyDJLK84hzqgWlryxt4p5eZjladgHyeKPlw",
+			},
+			"NewUser Invalid. Should return an error",
+			true,
+		},
+		{"Missing FullName",
+			&NewUser{
+				"test",
+				"",
+				"Test",
+				"$argon2id$v=19$m=65536,t=1," +
+					"p=2$9QWBW808Dq62aOfojSwBYg$muj80qzHXdB4DW06zgHbljhTLGrfFJ6qq8hDiK" +
+					"2esNTUBH0zlnBjGoDJ4S5glnKVCs9CD15cajhzkVxjgMwgHZdZ0JgZvUDcX9tJf8p7a" +
+					"TCOI8vN0vVVbZFbgUya6CFiqPH8cULMVyDJLK84hzqgWlryxt4p5eZjladgHyeKPlw",
+			},
+			"NewUser Valid. Should return nil",
+			false,
+		},
+		{"Missing DisplayName",
+			&NewUser{
+				"test",
+				"Test Tester",
+				"",
+				"$argon2id$v=19$m=65536,t=1," +
+					"p=2$9QWBW808Dq62aOfojSwBYg$muj80qzHXdB4DW06zgHbljhTLGrfFJ6qq8hDiK" +
+					"2esNTUBH0zlnBjGoDJ4S5glnKVCs9CD15cajhzkVxjgMwgHZdZ0JgZvUDcX9tJf8p7a" +
+					"TCOI8vN0vVVbZFbgUya6CFiqPH8cULMVyDJLK84hzqgWlryxt4p5eZjladgHyeKPlw",
+			},
+			"NewUser Valid. Should return nil",
+			false,
+		},
+		{"Missing EncodedHash",
+			&NewUser{
+				"test",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			"NewUser Invalid. Should return an error",
+			true,
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			errVNU := test.nu.ValidateNewUser()
 			if test.expectError && errVNU == nil {
 				t.Errorf("An error was expected, but did not occur\nSee detail:\n\t%s", test.detail)
+			} else if !test.expectError && errVNU != nil {
+				t.Errorf("An error was not expected, but one occured.\n\t"+
+					"Error: %s\n\tDetail: %s", errVNU, test.detail)
+			}
+		})
+	}
+}
+
+// TestNewUser_PrepNewUser is a unit test ensuring that PrepNewUser prepares all
+// fields that should be prepared.
+//
+// This test does not test all possible inputs; more exhaustive tests will
+// be run on the individual prep functions used by this method.
+func TestNewUser_PrepNewUser(t *testing.T) {
+	tests := []struct {
+		name       string
+		nu         *NewUser
+		expectedNu *NewUser
+		detail     string
+	}{
+		{"Basic Test",
+			&NewUser{
+				"test",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			&NewUser{
+				"test",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			"Both structs the same, all fields should equal.",
+		},
+		{"Missing Username",
+			&NewUser{
+				"",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			&NewUser{
+				"",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			"Both structs the same, all fields should equal.",
+		},
+		{"All Fields Missing",
+			&NewUser{
+				"",
+				"",
+				"",
+				"",
+			},
+			&NewUser{
+				"",
+				"",
+				"",
+				"",
+			},
+			"Both structs the same, all fields should equal.",
+		},
+		{"Extra Spaces around fields",
+			&NewUser{
+				" test  ",
+				" Test Tester   ",
+				" Test ",
+				"",
+			},
+			&NewUser{
+				"test",
+				"Test Tester",
+				"Test",
+				"",
+			},
+			"Both structs the same, all fields should equal.",
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			test.nu.PrepNewUser()
+			if test.nu.Username != test.expectedNu.Username {
+				t.Errorf("Usernames do not match. Got: %s, Expected: %s",
+					test.nu.Username, test.expectedNu.Username)
+			}
+			if test.nu.FullName != test.expectedNu.FullName {
+				t.Errorf("FullNames do not match. Got: %s, Expected: %s",
+					test.nu.FullName, test.expectedNu.FullName)
+			}
+			if test.nu.DisplayName != test.expectedNu.DisplayName {
+				t.Errorf("DisplayNames do not match. Got: %s, Expected: %s",
+					test.nu.DisplayName, test.expectedNu.DisplayName)
 			}
 		})
 	}
