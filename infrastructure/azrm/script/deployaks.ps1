110fda153065daa83be8f58904a569a297c38e86
Christopher Kites <chriskites@gmail.com>
2019-04-21 20:00:43 -0700

    Update az rm template for AKS AB#278

--- infrastructure/azrm/script/deployaks.ps1
+++ infrastructure/azrm/script/deployaks.ps1
@@ -1,5 +1,12 @@
+Write-Host "Creating service principal for aks cluster"
+
+$sp = New-AzADServicePrincipal -SkipAssignment
+Write-Host $sp.ApplicationId
+
 New-AzResourceGroupDeployment `
 -ResourceGroupName perceptiaAksCluster `
 -Name PerceptiaClusterDeployment `
 -TemplateFile ..\template\deployaks.json `
--TemplateParameterFile ..\template\deployaks.parameters.json
+-TemplateParameterFile ..\template\deployaks.parameters.json `
+-servicePrincipalClientId $(ConvertTo-SecureString $sp.ApplicationId.ToString() -AsPlainText -Force) `
+-servicePrincipalClientSecret $(ConvertTo-SecureString $sp.Secret.ToString() -AsPlainText -Force)
