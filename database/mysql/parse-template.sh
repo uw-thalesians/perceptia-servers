9d24d57e5789473a63f0780f88c734e5d86852bf
Ken Long <long27km@uw.edu>
2019-04-24 14:30:25 -0700

    fixup subsequent runs of mysql container, previous unstaged mods to REST README

--- database/mysql/parse-template.sh
+++ database/mysql/parse-template.sh
@@ -2,7 +2,10 @@
 echo "generating script with user pass from template"
 sed s/\{pass\}/$user_pass/ /docker-entrypoint-initdb.d/create_user.sql.template > /docker-entrypoint-initdb.d/create_user.sql
 echo "removing this script and template"
-rm /docker-entrypoint-initdb.d/create_user.sql.template /docker-entrypoint-initdb.d/parse-template.sh
-echo "returning to previous startup"
+rm /docker-entrypoint-initdb.d/create_user.sql.template /usr/local/bin/parse-template.sh
 
-/usr/local/bin/docker-entrypoint.sh mysqld
+#echo '/usr/local/bin/docker-entrypoint.sh "$@"' > /usr/local/bin/parse-template.sh
+echo "returning entrypoint to previous startup"
+ln -s /usr/local/bin/docker-entrypoint.sh /usr/local/bin/parse-template.sh
+
+/usr/local/bin/docker-entrypoint.sh "$@"
