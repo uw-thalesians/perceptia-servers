d888a26048f57178cdb104c4311179ae9c76e877
Christopher Kites <chriskites@gmail.com>
2019-04-25 23:23:25 -0700

    Add deploy script for gateway to cluster AB#268

--- infrastructure/azrm/script/deploypublicip.ps1
+++ infrastructure/azrm/script/deploypublicip.ps1
@@ -1,5 +1,5 @@
 New-AzResourceGroupDeployment `
--ResourceGroupName perceptiaAksCluster `
+-ResourceGroupName MC_perceptiaAksCluster_perceptiaCluster_westus2 `
 -Name PerceptiaPublicIpDeployment `
 -TemplateFile ..\template\deploypublicip.json `
 -TemplateParameterFile ..\template\deploypublicip.parameters.json
--- infrastructure/kube/script/addsecretcerts.ps1
+++ infrastructure/kube/script/addsecretcerts.ps1
@@ -1,2 +1,2 @@
-kubectl create secret tls api-tls --cert=$PERCEPTIA_SERVERS_SECRET\cert\fullchain.pem --key=$PERCEPTIA_SERVERS_SECRET\cert\privkey.pem --namespace production
-kubectl create secret tls api-tls --cert=$PERCEPTIA_SERVERS_SECRET\certdev\fullchain.pem --key=$PERCEPTIA_SERVERS_SECRET\certdev\privkey.pem --namespace development
+kubectl create secret tls api-tls --cert=$PERCEPTIA_SERVERS_SECRET\ssl\archive\fullchain1.pem --key=$PERCEPTIA_SERVERS_SECRET\ssl\archive\privkey1.pem --namespace production
+kubectl create secret tls api-tls --cert=$PERCEPTIA_SERVERS_SECRET\ssldev\archive\fullchain1.pem --key=$PERCEPTIA_SERVERS_SECRET\ssldev\archive\privkey1.pem --namespace development
