2e12d7b486a597020148cf6de9038623daed85ce
Christopher Kites <chriskites@gmail.com>
2019-05-14 22:03:59 -0700

    Add aqrest to aks deployment

--- infrastructure/kube/script/deploydev.ps1
+++ infrastructure/kube/script/deploydev.ps1
@@ -1,1 +1,9 @@
-kubectl apply -f .\..\deploy\common -f .\..\deploy\dev --namespace development
+Param (
+        [Switch]$DeleteDeployment
+)
+
+if ($DeleteDeployment) {
+        kubectl delete -f .\..\deploy\common -f .\..\deploy\dev --namespace development
+} else {
+        kubectl apply -f .\..\deploy\common -f .\..\deploy\dev --namespace development
+}
