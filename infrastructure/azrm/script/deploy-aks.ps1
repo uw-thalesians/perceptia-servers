914a69ff6394b8d702476f8f357d088c806d48f6
Christopher Kites <chriskites@gmail.com>
2019-05-16 20:29:26 -0700

    Update kube deployment for perceptia

--- infrastructure/azrm/script/deploy-aks.ps1
+++ infrastructure/azrm/script/deploy-aks.ps1
@@ -29,13 +29,7 @@
 
 param(
  [string]
- $subscriptionId = "845419c0-8828-43de-b0cf-793e05e113aa",
-
- [string]
  $resourceGroupName = "perceptiaAks",
-
- [string]
- $resourceGroupLocation = "westus2",
 
  [string]
  $deploymentName = "PerceptiaClusterDeployment",
@@ -44,24 +38,12 @@ param(
  $templateFilePath = ".\..\template\deployaks.json",
 
  [string]
- $parametersFilePath = ".\..\parameter\deployask.e2v3.json",
+ $parametersFilePath = ".\..\parameter\deployaks.e2sv3.json",
 
  [string]
  $servicePrincipalClientSecretFile = "$env:SECRET_PERCEPTIA_SERVERS\aks\sp\Secret.txt"
 )
 
-<#
-.SYNOPSIS
-    Registers RPs
-#>
-Function RegisterRP {
-    Param(
-        [string]$ResourceProviderNamespace
-    )
-
-    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
-    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
-}
 
 #******************************************************************************
 # Script body
@@ -69,44 +51,8 @@ Function RegisterRP {
 #******************************************************************************
 $ErrorActionPreference = "Stop"
 
-# sign in
-Write-Host "Logging in...";
-Login-AzureRmAccount;
-
-# select subscription
-Write-Host "Selecting subscription '$subscriptionId'";
-Select-AzureRmSubscription -SubscriptionID $subscriptionId;
-
-# Register RPs
-$resourceProviders = @("microsoft.containerservice","microsoft.resources");
-if($resourceProviders.length) {
-    Write-Host "Registering resource providers"
-    foreach($resourceProvider in $resourceProviders) {
-        RegisterRP($resourceProvider);
-    }
-}
-
-#Create or check for existing resource group
-$resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
-if(!$resourceGroup)
-{
-    Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
-    if(!$resourceGroupLocation) {
-        $resourceGroupLocation = Read-Host "resourceGroupLocation";
-    }
-    Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
-    New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
-}
-else{
-    Write-Host "Using existing resource group '$resourceGroupName'";
-}
-
 $SECRET = (Get-Content -Path $servicePrincipalClientSecretFile)
 
 # Start the deployment
 Write-Host "Starting deployment...";
-if(Test-Path $parametersFilePath) {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -servicePrincipalClientSecret $SECRET;
-} else {
-    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile $templateFilePath -servicePrincipalClientSecret $SECRET;
-}
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName -Name $deploymentName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -servicePrincipalClientSecret $SECRET;
--- infrastructure/azrm/script/deploy-public-ip.ps1
+++ infrastructure/azrm/script/deploy-public-ip.ps1
@@ -1,16 +1,25 @@
 # deploy-public-ip.ps1
 # Deploy public ip to be used by api services in cluster
+param(
+        [switch]$DeployProd,
+        [switch]$DeployDev
+)
 
-Write-Host "Deploying public ip named: api"
-New-AzResourceGroupDeployment `
--ResourceGroupName perceptiaApi `
--Name PerceptiaPublicIpApiDeployment `
--TemplateFile ..\template\deploypublicip.json `
--TemplateParameterFile ..\parameter\deploypublicip.api.json
+if ($DeployProd) {
+        Write-Host "Deploying public ip named: api"
+        New-AzResourceGroupDeployment `
+        -ResourceGroupName perceptiaApi `
+        -Name PerceptiaPublicIpApiDeployment `
+        -TemplateFile ..\template\deploypublicip.json `
+        -TemplateParameterFile ..\parameter\deploypublicip.api.json
+}
 
-Write-Host "Deploying public ip named: api-dev"
-New-AzResourceGroupDeployment `
--ResourceGroupName perceptiaApi `
--Name PerceptiaPublicIpApiDevDeployment `
--TemplateFile ..\template\deploypublicip.json `
--TemplateParameterFile ..\parameter\deploypublicip.api.dev.json
+if ($DeployDev) {
+        Write-Host "Deploying public ip named: api-dev"
+        New-AzResourceGroupDeployment `
+        -ResourceGroupName perceptiaApi `
+        -Name PerceptiaPublicIpApiDevDeployment `
+        -TemplateFile ..\template\deploypublicip.json `
+        -TemplateParameterFile ..\parameter\deploypublicip.api.dev.json
+}
+
