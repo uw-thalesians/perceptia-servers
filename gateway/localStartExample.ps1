a157cb6c64d45f2944337334a09b5d5728c5edf0
Christopher Kites <chriskites@gmail.com>
2019-05-25 00:07:40 -0700

    Update scripts to use build 326

--- gateway/localStartExample.ps1
+++ gateway/localStartExample.ps1
@@ -1,13 +1,13 @@
 Param (
     [switch]$Latest,
-    [string]$Build = "316",
+    [string]$Build = "326",
     [String]$Branch = "develop",
     [switch]$CurrentBranch,
 
     [String]$PerceptiaDockerNet = "perceptia-net",
 
     [switch]$BuildGateway = $false,
-    [String]$GatewayVersion = "1.0.0",
+    [String]$GatewayVersion = "1",
     [String]$GatewayPortPublish = "4443",
 
     [switch]$SkipMsSql = $false,
@@ -41,11 +41,14 @@ Set-Variable -Name DOCKERHUB_ORG -Value "uwthalesians"
 
 Set-Variable -Name LATEST_COMMIT -Value "$(git rev-parse --short HEAD)"
 
+Set-Variable -Name DOCKER_NETWORK -Value $PerceptiaDockerNet
+
 ## Gateway Veriables
 Set-Variable -Name GATEWAY_SERVICE_NAME -Value "gateway-lc-svc"
 
 Set-Variable -Name GATEWAY_IMAGE_NAME -Value "gateway"
 Set-Variable -Name GATEWAY_CONTAINER_NAME -Value "gateway"
+Set-Variable -Name GATEWAY_PORT_PUBLISH -Value $GatewayPortPublish
 
 
 Set-Variable -Name GATEWAY_TLSCERTPATH -Value "/encrypt/fullchain.pem"
@@ -59,18 +62,27 @@ Set-Variable -Name GATEWAY_API_PORT -Value "$GatewayPortPublish"
 ## Redis Variables
 Set-Variable -Name REDIS_CONTAINER_NAME -Value $RedisHost
 Set-Variable -Name REDIS_VOLUME_NAME -Value "${GATEWAY_SERVICE_NAME}_redis_vol"
+Set-Variable -Name REDIS_PORT_PUBLISH -Value $RedisPortPublish
+Set-Variable -Name REDIS_PORT -Value $RedisPort
+
 
 ## MsSql Variables
-Set-Variable -Name MSSQL_DATABASE -Value $MsSqlDatabase
-Set-Variable -Name MSSQL_HOST -Value $MsSqlHost
-Set-Variable -Name MSSQL_PASSWORD -Value $MsSqlGatewaySpPassword
-Set-Variable -Name MSSQL_PORT -Value $MsSqlPort
-Set-Variable -Name MSSQL_SCHEME -Value $MsSqlScheme
-Set-Variable -Name MSSQL_USERNAME -Value $MsSqlGatewaySpUsername
-
 Set-Variable -Name MSSQL_IMAGE_NAME -Value "mssql"
 Set-Variable -Name MSSQL_CONTAINER_NAME -Value "mssql"
 Set-Variable -Name MSSQL_VOLUME_NAME -Value "${GATEWAY_SERVICE_NAME}_mssql_vol"
+Set-Variable -Name MSSQL_PORT_PUBLISH -Value $MsSqlPortPublish
+
+Set-Variable -Name MSSQL_SA_PASSWORD -Value $MsSqlSaPassword
+Set-Variable -Name MSSQL_GATEWAY_SP_USERNAME -Value $MsSqlGatewaySpUsername
+Set-Variable -Name MSSQL_GATEWAY_SP_PASSWORD -Value $MsSqlGatewaySpPassword
+
+Set-Variable -Name MSSQL_DATABASE -Value $MsSqlDatabase
+Set-Variable -Name MSSQL_HOST -Value $MsSqlHost
+Set-Variable -Name MSSQL_PORT -Value $MsSqlPort
+Set-Variable -Name MSSQL_SCHEME -Value $MsSqlScheme
+
+
+
 
 if (!$CleanUp) {
 
@@ -155,9 +167,9 @@ if (!$CleanUp) {
         docker volume rm $REDIS_VOLUME_NAME
     }
     
-    if (!((docker network ls) -Match $PerceptiaDockerNet)) {
-        Write-Host "Docker network $PerceptiaDockerNet does not exist, creating now..."
-        docker network create -d bridge $PerceptiaDockerNet 
+    if (!((docker network ls) -Match $DOCKER_NETWORK)) {
+        Write-Host "Docker network $DOCKER_NETWORK does not exist, creating now..."
+        docker network create -d bridge $DOCKER_NETWORK 
         Write-Host "`n"
     }
     
@@ -184,13 +196,13 @@ if (!$CleanUp) {
         --label "label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}" `
         --label "label.perceptia.info/sub-of=${GATEWAY_SERVICE_NAME}" `
         --name $REDIS_CONTAINER_NAME `
-        --network $PerceptiaDockerNet `
-        --publish "${RedisPortPublish}:6379" `
+        --network $DOCKER_NETWORK `
+        --publish "${REDIS_PORT_PUBLISH}:6379" `
         --mount type=volume,source=${REDIS_VOLUME_NAME},destination=/data `
         redis:5.0.4-alpine
         Write-Host "`n"
-        Write-Host "Redis Server is listening inside docker network: ${PerceptiaDockerNet} at: ${REDIS_CONTAINER_NAME}:6379"
-        Write-Host "Redis Server is listening on the host at: localhost:${RedisPortPublish}"
+        Write-Host "Redis Server is listening inside docker network: ${DOCKER_NETWORK} at: ${REDIS_CONTAINER_NAME}:6379"
+        Write-Host "Redis Server is listening on the host at: localhost:${REDIS_PORT_PUBLISH}"
         Write-Host "`n"
     } else {
         Write-Host "Be sure to start mssql dependency, see README"
@@ -208,19 +220,21 @@ if (!$CleanUp) {
         Write-Host "Using mssql image: $MSSQL_IMAGE_AND_TAG"
 
         # Check if image exists
+        Write-Host "Checking Dockerhub for image..."
         (docker pull $MSSQL_IMAGE_AND_TAG) | Out-Null 
         if (!$?) {
                 Write-Host "Image: $MSSQL_IMAGE_AND_TAG not found on dockerhub, exiting"
                 exit(1)
         }
+        Write-Host "Mssql image found!"
     
         docker run `
         --detach `
         --env 'ACCEPT_EULA=Y' `
-        --env "GATEWAY_SP_USERNAME=${MSSQL_USERNAME}" `
-        --env "GATEWAY_SP_PASSWORD=${MSSQL_PASSWORD}" `
+        --env "GATEWAY_SP_USERNAME=${MSSQL_GATEWAY_SP_USERNAME}" `
+        --env "GATEWAY_SP_PASSWORD=${MSSQL_GATEWAY_SP_PASSWORD}" `
         --env "MSSQL_ENVIRONMENT=development" `
-        --env "SA_PASSWORD=$MsSqlSaPassword" `
+        --env "SA_PASSWORD=$MSSQL_SA_PASSWORD" `
         --env "SKIP_SETUP_IF_EXISTS=Y" `
         --label "label.perceptia.info/name=${MSSQL_CONTAINER_NAME}" `
         --label "label.perceptia.info/instance=${MSSQL_CONTAINER_NAME}-1" `
@@ -230,24 +244,26 @@ if (!$CleanUp) {
         --label "label.perceptia.info/sub-of=${GATEWAY_SERVICE_NAME}" `
         --mount type=volume,source=${MSSQL_VOLUME_NAME},destination=/var/opt/mssql `
         --name=${MSSQL_CONTAINER_NAME} `
-        --network $PerceptiaDockerNet `
-        --publish "${MsSqlPortPublish}:1433" `
+        --network $DOCKER_NETWORK `
+        --publish "${MSSQL_PORT_PUBLISH}:1433" `
         ${MSSQL_IMAGE_AND_TAG}
         Write-Host "`n"
-        Write-Host "MsSql Server is listening inside docker network: ${PerceptiaDockerNet} at: ${MSSQL_CONTAINER_NAME}:1433"
-        Write-Host "MsSql Server is listening on the host at: localhost:${MsSqlPortPublish}"
+        Write-Host "MsSql Server is listening inside docker network: ${DOCKER_NETWORK} at: ${MSSQL_CONTAINER_NAME}:1433"
+        Write-Host "MsSql Server is listening on the host at: localhost:${MSSQL_PORT_PUBLISH}"
         Write-Host "`n"
     } else {
         Write-Host "Be sure to start mssql dependency, see README"
     }
-
+    Write-Host "Using gateway image: $GATEWAY_IMAGE_AND_TAG"
     if (!$BuildGateway) {
+        Write-Host "Checking Dockerhub for image..."
         # Check if image exists
         (docker pull $GATEWAY_IMAGE_AND_TAG) | Out-Null 
         if (!$?) {
                 Write-Host "Image: $GATEWAY_IMAGE_AND_TAG not found on dockerhub, exiting"
                 exit(1)
         }
+        Write-Host "Gateway image found!"
     }
     
     docker run `
@@ -261,29 +277,39 @@ if (!$CleanUp) {
     --env GATEWAY_TLSKEYPATH="$GATEWAY_TLSKEYPATH" `
     --env MSSQL_DATABASE="$MSSQL_DATABASE" `
     --env MSSQL_HOST="$MSSQL_HOST" `
-    --env MSSQL_PASSWORD="$MSSQL_PASSWORD" `
+    --env MSSQL_GATEWAY_SP_PASSWORD="$MSSQL_GATEWAY_SP_PASSWORD" `
     --env MSSQL_PORT="$MSSQL_PORT" `
     --env MSSQL_SCHEME="$MSSQL_SCHEME" `
-    --env MSSQL_USERNAME="$MSSQL_USERNAME" `
-    --env REDIS_ADDRESS="${RedisHost}:$RedisPort" `
+    --env MSSQL_GATEWAY_SP_USERNAME="$MSSQL_GATEWAY_SP_USERNAME" `
+    --env REDIS_ADDRESS="${REDIS_CONTAINER_NAME}:$REDIS_PORT" `
     --label "label.perceptia.info/name=${GATEWAY_CONTAINER_NAME}" `
     --label "label.perceptia.info/instance=gateway-1" `
     --label "label.perceptia.info/managed-by=docker" `
     --label "label.perceptia.info/component=server" `
     --label "label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}" `
     --name ${GATEWAY_CONTAINER_NAME} `
-    --network $PerceptiaDockerNet `
-    --publish "${GatewayPortPublish}:443" `
+    --network $DOCKER_NETWORK `
+    --publish "${GATEWAY_PORT_PUBLISH}:443" `
     --restart on-failure `
     --mount type=bind,source=$GATEWAY_TLSMOUNTSOURCE,target=/encrypt/,readonly `
     ${GATEWAY_IMAGE_AND_TAG}
     
     Write-Host "`n"
-    Write-Host "Gateway is listening at https://localhost:${GatewayPortPublish}"
+    Write-Host "Gateway is listening at https://localhost:${GATEWAY_PORT_PUBLISH}"
+    Write-Host "`n"
+    Write-Host "To test if the gateway is able to process requests, make a GET request to:"
+    Write-Host "/api/v1/gateway/health"
+    Write-Host "Example request using curl"
+    Write-Host "curl --insecure -X GET `"https://localhost:${GATEWAY_PORT_PUBLISH}/api/v1/gateway/health`""
+    Write-Host "`n"
+    Write-Host "Mssql server for the Perceptia databased used by the gateway can be reached at port: $MSSQL_PORT_PUBLISH"
+    Write-Host "Perceptia databased used by the gateway has an 'sa' user account with password: $MSSQL_SA_PASSWORD"
+    Write-Host "`n"
     docker ps --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}"
 } else {
 
     if ((docker ps -aq --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}").Length -gt 0) {
+        Write-Host "Removing all contaienrs started by this script..."
         docker rm --force (docker ps -aq --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")
     }
     if ($RemoveAllDbVolumes -and (((docker volume ls --format "{{.Name}}") -Match "${GATEWAY_SERVICE_NAME}"))) {
@@ -292,25 +318,30 @@ if (!$CleanUp) {
                 Write-Host "Removing all containers before removing volumes"
                 docker rm --force (docker ps -aq --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")
         }  
+        Start-Sleep -Seconds 2
         docker volume rm (docker volume ls --format "{{.Name}}" --filter "name=${GATEWAY_SERVICE_NAME}")
     }
     if (($MsSqlRemoveDbVolume) -and (((docker volume ls --format "{{.Name}}") -Match "$MSSQL_VOLUME_NAME" ))){
         Write-Host "-MsSqlRemoveDbVolume option set, removing volume: $MSSQL_VOLUME_NAME"
         if ((docker ps -aq --filter "label=label.perceptia.info/name=mssql" --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")) {
-                docker rm --force (docker ps -aq --filter "label=label.perceptia.info/name=mssql"  --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")
+            Write-Host "Removing any mssql containers started by this script to allow removing the database volume: $MSSQL_VOLUME_NAME"
+            docker rm --force (docker ps -aq --filter "label=label.perceptia.info/name=mssql"  --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")
         } 
         Start-Sleep -Seconds 2
+        Write-Host "Removing mssql volume..."
         docker volume rm $MSSQL_VOLUME_NAME
     }
     if (($RedisRemoveDbVolume) -and (((docker volume ls --format "{{.Name}}") -Match "$REDIS_VOLUME_NAME" ))) {
         Write-Host "-RedisRemoveDbVolume option set, removing volume: $REDIS_VOLUME_NAME"
         if ((docker ps -aq --filter "label=label.perceptia.info/name=redis" --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAME}")) {
-                docker rm --force (docker ps -aq --filter "label=label.perceptia.info/name=redis"  --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAMEE}")
+            Write-Host "Removing any redis containers started by this script to allow removing the database volume: $REDIS_VOLUME_NAME"
+            docker rm --force (docker ps -aq --filter "label=label.perceptia.info/name=redis"  --filter "label=label.perceptia.info/part-of=${GATEWAY_SERVICE_NAMEE}")
         } 
         
         Start-Sleep -Seconds 2
+        Write-Host "Removing redis volume..."
         docker volume rm $REDIS_VOLUME_NAME
     }
-    Write-Host "Complete!"
+    Write-Host "CleanUp Complete!"
 }
 
--- infrastructure/kube/script/add-secrets.ps1
+++ infrastructure/kube/script/add-secrets.ps1
@@ -4,6 +4,10 @@ Set-Variable -Name GATEWAY_SESSION_KEY -Value (Get-Content -Path $Env:SECRET_PER
 Set-Variable -Name MSSQL_SA_PASSWORD -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keys\MssqlSaPassword.txt)
 Set-Variable -Name MSSQL_GATEWAY_SP_PASSWORD -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keys\MssqlGatewaySpPassword.txt)
 Set-Variable -Name MSSQL_GATEWAY_SP_USERNAME -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keys\MssqlGatewaySpUsername.txt)
+Set-Variable -Name GATEWAY_API_SCHEME -Value "https"
+Set-Variable -Name GATEWAY_API_HOST -Value "api.perceptia.info"
+Set-Variable -Name GATEWAY_API_PORT -Value "443"
+
 
 Set-Variable -Name AQMYSQL_ANYQUIZ_USER_PASSWORD_DEV -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keysdev\AqmysqlAnyQuizUserPassword.txt)
 Set-Variable -Name AQMYSQL_ROOT_PASSWORD_DEV -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keysdev\AqmysqlRootPassword.txt)
@@ -11,6 +15,10 @@ Set-Variable -Name GATEWAY_SESSION_KEY_DEV -Value (Get-Content -Path $Env:SECRET
 Set-Variable -Name MSSQL_SA_PASSWORD_DEV -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keysdev\MssqlSaPassword.txt)
 Set-Variable -Name MSSQL_GATEWAY_SP_PASSWORD_DEV -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keysdev\MssqlGatewaySpPassword.txt)
 Set-Variable -Name MSSQL_GATEWAY_SP_USERNAME_DEV -Value (Get-Content -Path $Env:SECRET_PERCEPTIA_SERVERS\keysdev\MssqlGatewaySpUsername.txt)
+Set-Variable -Name GATEWAY_API_SCHEME_DEV -Value "https"
+Set-Variable -Name GATEWAY_API_HOST_DEV -Value "api.dev.perceptia.info"
+Set-Variable -Name GATEWAY_API_PORT_DEV -Value "443"
+
 
 
 
@@ -23,6 +31,9 @@ kubectl create secret generic aqmysql --type=string `
 
 kubectl create secret generic gateway --type=string `
 --from-literal=session-key=$GATEWAY_SESSION_KEY `
+--from-literal=api-scheme=$GATEWAY_API_SCHEME `
+--from-literal=api-host=$GATEWAY_API_HOST `
+--from-literal=api-port=$GATEWAY_API_PORT `
 --namespace production 
 
 kubectl create secret generic mssql --type=string `
@@ -40,6 +51,9 @@ kubectl create secret generic aqmysql --type=string `
 
 kubectl create secret generic gateway --type=string `
 --from-literal=session-key=$GATEWAY_SESSION_KEY_DEV `
+--from-literal=api-scheme=$GATEWAY_API_SCHEME_DEV `
+--from-literal=api-host=$GATEWAY_API_HOST_DEV `
+--from-literal=api-port=$GATEWAY_API_PORT_DEV `
 --namespace development
 
 kubectl create secret generic mssql --type=string `
