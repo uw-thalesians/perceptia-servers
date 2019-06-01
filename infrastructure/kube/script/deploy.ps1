567977ead11fa06b5bd3493110aeba1dd176f342
Christopher Kites <chriskites@gmail.com>
2019-06-01 14:32:37 -0700

    Add pvc for redis and mssql services

--- infrastructure/kube/script/deploy.ps1
+++ infrastructure/kube/script/deploy.ps1
@@ -4,23 +4,36 @@ Param (
         [switch]$Prod
 )
 
-Set-Variable -Name NAMESPACE -Value "development"
-Set-Variable -Name DEPLOY_DIR -Value "dev"
+
 if ($Prod) {
         Set-Variable -Name NAMESPACE -Value "production" 
         Set-Variable -Name DEPLOY_DIR -Value "prod"
+        Write-Host "Prod switch set, using $NAMESPACE namespace for all actions..."
+} else {
+        Set-Variable -Name NAMESPACE -Value "development"
+        Set-Variable -Name DEPLOY_DIR -Value "dev"
+        Write-Host "Prod switch not set, defaulting to $NAMESPACE namespace for all actions..."
 }
 
 
 
 if ($DeletePVC) {
+        Write-Host "`n"
+        Write-Host "DeletePVC switch set, deleting all PVCs created with the ./../setup/pvc.yaml config..."
+        Write-Host "`n"
         kubectl delete -f "./../setup/pvc.yaml" --namespace $NAMESPACE
+} else {
+        Write-Host "`n"
+        Write-Host "DeletePVC switch not set, existing PVCs created with the ./../setup/pvc.yaml config will be reused..."
+        Write-Host "`n"
 }
 
 if ($DeleteDeployment) {
+        Write-Host "`n"
         Write-Host "Deleting Deployment for namespace: $NAMESPACE"
         kubectl delete -f "./../deploy/common" -f "./../deploy/$DEPLOY_DIR" --namespace $NAMESPACE
 } else {
+        Write-Host "`n"
         Write-Host "Deploying for namespace: $NAMESPACE"
         kubectl apply -f "./../setup/pvc.yaml" -f "./../deploy/common" -f "./../deploy/$DEPLOY_DIR" --namespace $NAMESPACE
 }
